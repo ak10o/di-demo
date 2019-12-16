76c56f5a77cb54815cdc4df5ffc3fcbd1f640528
Avinash kumar <avinash369kumar@gmail.com>
2019-12-16 07:07:28 +0530

    printing hello in the console

--- src/main/java/com/mbrdi/didemo/DiDemoApplication.java
+++ src/main/java/com/mbrdi/didemo/DiDemoApplication.java
@@ -2,12 +2,18 @@ package com.mbrdi.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.mbrdi.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		
+		MyController controller = (MyController)ctx.getBean("myController");
+		System.out.println(controller.hello());
 	}
 
 }
---
+++ src/main/java/com/mbrdi/didemo/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.mbrdi.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+	public String hello() {
+		return "hello";
+	}
+}
