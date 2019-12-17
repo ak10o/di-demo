88180371e72658733cefa01ebe635247867152c3
Avinash kumar <avinash369kumar@gmail.com>
2019-12-18 04:36:16 +0530

    Profile example

--- src/main/java/com/mbrdi/didemo/controllers/PrimaryGreetingService.java
+++ src/main/java/com/mbrdi/didemo/controllers/PrimaryGreetingService.java
@@ -1,12 +1,15 @@
 package com.mbrdi.didemo.controllers;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.mbrdi.didemo.services.GreetingService;
 
 @Service
 @Primary
+// If we don't write the Profile then it will throw an error saying NoUniqueBeanDefinitionException
+@Profile("en")
 public class PrimaryGreetingService implements GreetingService {
 
 	@Override
