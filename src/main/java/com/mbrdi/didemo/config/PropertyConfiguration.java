8704f92ae4bb19b1d23705e4c5129eb34cdfe0b3
Avinash kumar <avinash369kumar@gmail.com>
2020-04-23 18:16:13 +0530

    refactored code
    instead of using datasource/jms properties file, we will be using application.properties to set the custom property.

--- src/main/java/com/mbrdi/didemo/config/PropertyConfiguration.java
+++ src/main/java/com/mbrdi/didemo/config/PropertyConfiguration.java
@@ -4,61 +4,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 import com.mbrdi.didemo.externalization.FakeDataSource;
 import com.mbrdi.didemo.externalization.FakeJmsBroker;
 
 @Configuration
-@PropertySources({
-	@PropertySource("datasource.properties"),
-	@PropertySource("jms.properties")
-})
-
 public class PropertyConfiguration {
 
-	@Autowired
-	Environment env;
-	
 	@Value("${guru.username}")
 	String user;
-	
+
 	@Value("${guru.password}")
 	String password;
-	
+
 	@Value("${guru.dburl}")
 	String url;
-	
+
 	@Value("${guru.jms.username}")
 	String jmsUser;
-	
+
 	@Value("${guru.jms.password}")
 	String jmsPassword;
-	
+
 	@Value("${guru.jms.url}")
 	String jmsUrl;
-	
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyConfig() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-	
+
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
 		fakeDataSource.setUserName(user);
-		if (env.getProperty("pwd").isEmpty()) {
-			fakeDataSource.setPassword(password);
-		} else {
-			fakeDataSource.setPassword(env.getProperty("pwd"));
-		}
+		fakeDataSource.setPassword(password);
 		fakeDataSource.setDburl(url);
 		return fakeDataSource;
 	}
-	
+
 	@Bean
 	public FakeJmsBroker fakeJmsBroker() {
 		FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
